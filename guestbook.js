2e89cd9e364606aae4cdcf44af26cc860db1d8e8
AlfredoChusgo <87032218+AlfredoChusgo@users.noreply.github.com>
2022-09-21 00:36:00 -0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/ee41e38d-d2b2-4e90-8aa7-1057587fde97/guestbook/read-guestbook-entries-sequence.json';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/ee41e38d-d2b2-4e90-8aa7-1057587fde97/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
